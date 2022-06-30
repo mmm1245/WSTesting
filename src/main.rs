910fb61e40fb26db719da61d90f5eff8c5f35e9c
mmm <aaa123@pobox.sk>
2022-06-30 23:27:21 +0200

    first commit

---
+++ src/main.rs
@@ -0,0 +1,101 @@
+use std::fs::{File};
+use std::io::{BufRead, BufReader};
+use std::net::TcpStream;
+use tungstenite::{connect, Message, WebSocket};
+use tungstenite::stream::MaybeTlsStream;
+use url::Url;
+use std::env;
+use std::path::PathBuf;
+use glob::{glob_with, MatchOptions};
+
+fn is_path_file(path : &PathBuf) -> bool {
+    let meta = path.metadata();
+    if let Ok(meta) = meta {
+        return meta.is_file();
+    }
+    false
+}
+
+fn main() {
+    let mut args = env::args();
+    let ws_url = Url::parse(args.nth(1).unwrap().as_str()).unwrap();
+    let glob_pattern = args.next().unwrap();
+
+    let glob_options = MatchOptions {
+        case_sensitive: true,
+        require_literal_separator: true,
+        require_literal_leading_dot: false,
+    };
+    let mut test_results : Vec<(String,Result<(),String>)> = Vec::new();
+    for entry in glob_with(glob_pattern.as_str(), glob_options).expect("Invalid glob pattern") {
+        if let Ok(path) = entry {
+            if !is_path_file(&path) {
+                continue;
+            }
+            test_results.push((path.display().to_string(),run_test(ws_url.clone(), File::open(&path).expect(format!("Could not open test file {}", path.display()).as_str()))));
+        }
+    }
+    println!("{}/{} tests passed", test_results.iter().filter(|e| e.1.is_ok()).count(), test_results.len());
+    for result in test_results {
+        if let Err(reason) = result.1 {
+            println!("{} failed with: {}", result.0, reason);
+        }
+    }
+}
+
+fn run_test(url : Url, file : File) -> Result<(),String>{
+    let (mut socket, _) = connect(url).map_err(|_| "Can't connect")?;
+    let file = BufReader::new(file);
+    for line in file.lines() {
+        if let Ok(line) = line {
+            let mut chars = line.chars();
+            let cmd = chars.next();
+            let sep = chars.next();
+            if let Some(sep) = sep {
+                if sep != ':' {
+                    return Err("Separator should be ':'".to_string());
+                }
+            } else {
+                return Err("Separator not found".to_string());
+            }
+            if let Some(cmd) = cmd {
+                let data = chars.as_str();
+                match cmd {
+                    'S' => {
+                        let send_result = socket.write_message(Message::Text(data.to_string()));
+                        if let Err(_) = send_result {
+                            return Err("Couldnt send message(probably closed)".to_string());
+                        }
+                    }
+                    'E' => {
+                        let text = read_text(& mut socket).unwrap();
+                        if text != data {
+                            return Err(format!("\n\texpected {} \n\tgot {}", data, text));
+                        }
+                    }
+                    '#' => {}
+                    _ => {
+                        return Err(format!("Unknown cmd {}", cmd));
+                    }
+                }
+            }
+        }
+    }
+    Ok(())
+}
+
+fn read_text(socket : &mut WebSocket<MaybeTlsStream<TcpStream>>) -> Result<String,&str> {
+    loop {
+        let read_val = match socket.read_message().unwrap() {
+            Message::Text(text) => {Some(Ok(text))}
+            Message::Binary(_) => {Some(Err("Binary frames not supported"))}
+            Message::Ping(_) => {None}
+            Message::Pong(_) => {None}
+            Message::Close(_) => {Some(Err("Websocket closed"))}
+            Message::Frame(_) => unreachable!()
+        };
+        if let Some(read_val) = read_val {
+            return read_val
+        }
+    }
+}
